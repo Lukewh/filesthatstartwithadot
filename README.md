a0d7e2ebd1e4df992865ead653b95de336804d21
Lukewh <luke@lukewh.com>
2018-05-15 15:28:05 +0000

    Initial files

---
+++ README.md
@@ -0,0 +1,7 @@
+# filesthatstartwithadot
+
+My files that start with a dot. In this repo all files and folders don't start with dots, so they're easier to browse.
+
+## Install
+
+`./symlink.sh` symlinks all the files in `~/`
---
+++ symlink.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+ln -s $PWD/bash_logout ~/.bash_logout
+ln -s $PWD/bashrc ~/.bashrc
+ln -s $PWD/oh-my-zsh ~/.oh-my-zsh
+ln -s $PWD/profile ~/.profile
+ln -s $PWD/vim ~/.vim
+ln -s $PWD/xinit ~/.xinit
+ln -s $PWD/zshrc ~/.zshrc
+
+ln -s $PWD/config/i3 ~/.config/i3
+ln -s $PWD/config/lxterminal ~/.config/lxterminal
+ln -s $PWD/config/Thunar ~/.config/Thunar
+ln -s $PWD/config/xfce4 ~/.config/xfce4
